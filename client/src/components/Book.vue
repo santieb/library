8c54fefb4b3604805b2eeb188bb8fcef4c3d0b1c
santieb <santiagonicolasbarreto@gmail.com>
2023-11-01 22:31:48 -0300

    chore: truncate sinopsis

--- client/src/components/Book.vue
+++ client/src/components/Book.vue
@@ -2,7 +2,7 @@
   <div class="max-w-sm rounded overflow-hidden shadow-lg">
     <div class="px-6 py-4">
       <div class="font-bold text-xl mb-2">{{ title }}</div>
-      <p class="text-gray-700 text-base">{{ synopsis }}</p>
+      <p class="text-gray-700 text-base">{{ truncateString(synopsis) }}</p>
     </div>
     <div class="px-6 pt-4 pb-2">
       <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{{
@@ -16,6 +16,8 @@
 </template>
 
 <script>
+import { truncateString } from '../helpers'
+
 export default {
   props: {
     title: String,
@@ -24,6 +26,11 @@ export default {
     pages: Number,
     synopsis: String,
     coverImage: String,
-  }
+  },
+  methods: {
+    truncateString
+  },
 }
+
 </script>
+
---
+++ client/src/helpers/index.js
@@ -0,0 +1,3 @@
+export const truncateString = (string) => {
+  return `${string.substring(0, 50)}...`
+}
